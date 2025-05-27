e737fcf9179af9f1b50d3fa62f755f5b6a5d9c88
Fixted <96688772+Fixted@users.noreply.github.com>
2025-05-27 20:58:28 +0200

    Add files via upload

---
+++ Proiect C.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int numar_secret, incercare, numar_incercari = 0;
+
+
+    srand(time(0));
+    numar_secret = rand() % 100 + 1;
+
+    printf("Ghiceste numarul (intre 1 si 100):\n");
+
+    do {
+        printf("Introdu o valoare: ");
+        scanf("%d", &incercare);
+        numar_incercari++;
+
+        if (incercare < numar_secret) {
+            printf("Prea mic!\n");
+        } else if (incercare > numar_secret) {
+            printf("Prea mare!\n");
+        } else {
+            printf("Felicitari! Ai ghicit numarul in %d incercari!\n", numar_incercari);
+        }
+
+    } while (incercare != numar_secret);
+
+    return 0;
+}
